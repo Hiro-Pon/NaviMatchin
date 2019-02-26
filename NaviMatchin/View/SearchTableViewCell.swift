8ffae1dfaa88ee5263fc48f4860b20a2708ec231
Kenty <>
2019-02-26 17:22:14 +0900

    PiroYaKentyPon THE FINAL

---
+++ NaviMatchin/View/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  NaviMatchin
+//
+//  Created by Kento Katsumata on 2019/02/26.
+//  Copyright © 2019 hiropon. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
